969c8a3769a5b01f94f6cfdd00e6304d9382aca9
tadarsh <adarshsomeshwaralok@gmail.com>
2025-06-30 23:37:29 +0530

    '/' issue

--- frontend/src/utils/checkAuthAndRefresh.js
+++ frontend/src/utils/checkAuthAndRefresh.js
@@ -1,7 +1,7 @@
 // src/utils/checkAuthAndRefresh.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/';
+const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const checkAuthAndRefresh = async () => {
   const access = localStorage.getItem('access_token');   // ✅ consistent key
