295a4128358c5e43cafad6a4333d81a5f7c052d5
tadarsh <adarshsomeshwaralok@gmail.com>
2025-07-01 16:31:04 +0530

    all set let's see

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 echo "📦 Installing dependencies..."
 pip install -r requirements.txt
 
-echo "⬇️ Downloading NLTK punkt..."
-python -c "import nltk; nltk.download('punkt'); nltk.download('punkt_tab')"
+echo "⬇️ Downloading required NLTK data..."
+python -c "import nltk; nltk.download('punkt')"
 
 # Collect static files (optional, if you're using Django admin or serving static)
 python manage.py collectstatic --noinput
