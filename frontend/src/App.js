8a318107713fae6f8c013ce03f4e259299976d49
tadarsh <adarshsomeshwaralok@gmail.com>
2025-07-05 19:38:49 +0530

    using browserrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import Navbar from './components/Navbar';
