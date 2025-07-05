5668e033c8470560611488248d6331ade9cdb992
tadarsh <adarshsomeshwaralok@gmail.com>
2025-07-05 23:53:12 +0530

    unnecessary import removed

--- backend/notes_app/urls.py
+++ backend/notes_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import NoteListCreateView, NoteDetailView,  summarize_note, upload_pdf, RequestPasswordResetView, ResetPasswordConfirmView
+from .views import NoteListCreateView, NoteDetailView,  summarize_note, upload_pdf
 
 urlpatterns = [
     path('notes/', NoteListCreateView.as_view(), name='note-list-create'),
@@ -7,7 +7,6 @@ urlpatterns = [
     path('summarize/', summarize_note, name='summarize'),
     # path for upload pdf's for summarizing feature...
     path('upload-pdf/', upload_pdf, name='upload_pdf'),
-    path('password-reset/', RequestPasswordResetView.as_view(), name='password_reset'),
-    path('password-reset-confirm/', ResetPasswordConfirmView.as_view(), name='password_reset_confirm'),
+    
 
 ]
